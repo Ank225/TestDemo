649a10f1bb6f0ae341f7c171e426aa0f06279c98
Ank225 <60406584+Ank225@users.noreply.github.com>
2021-05-19 16:13:38 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@ FILE=my-key.jks
 STRING='AccessDenied'
 if [[ ! -z $(grep "$STRING" "$FILE") ]];
 then
+  rm my-key.jks
   echo "key store not found, generating"
   keytool -genkeypair -dname "cn=Sachin, ou=EnggTeam, o=Bikayi, c=IN" -alias my-alias -keypass 123456 -keystore my-key.jks -keyalg DSA -storepass 123456 -validity 10000
 else
